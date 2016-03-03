00add95066af71951c77bbd78daaf7d472e413e0
Jatin Soni <dev@q2amarket.com>
2016-03-03 20:16:53 +0400

    Fixed option bloat issue with added more places with version update to 1.3

--- qa-plugin.php
+++ qa-plugin.php
@@ -5,17 +5,15 @@
 	Plugin URI: http://store.q2amarket.com/simple-advert/
 	Plugin Update Check URI: https://github.com/q2amarket/q2am-simple-adverts/raw/master/qa-plugin.php
 	Plugin Description: Add recent questions widget on sidebar or template area
-	Plugin Version: 1.2
-	Plugin Date: 2014-01-10
+	Plugin Version: 1.3
+	Plugin Date: 2016-03-03
 	Plugin Author: Q2A Market
 	Plugin Author URI: http://www.q2amarket.com
 	Plugin License: GPLv2
-	Plugin Minimum Question2Answer Version: 1.6
+	Plugin Minimum Question2Answer Version: 1.7
 */
 
 if (!defined('QA_VERSION')){header('Location: ../../'); exit;}
 
 qa_register_plugin_layer('qa-adverts-layer.php', 'Q2A Market - Simple Adverts');
 qa_register_plugin_module('module', 'qa-adverts-options.php', 'qa_adverts', 'Q2A Market - Simple Adverts Options');
-	
-?>
